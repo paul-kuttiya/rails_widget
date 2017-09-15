20197d0b727d472c74c2247486479f7a3762cc93
pookzzz2231 <w.kuttiya@gmail.com>
2017-09-15 00:44:47 -0400

    revise route

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,10 +1,4 @@
 class WidgetsController < ApplicationController
   protect_from_forgery except: :flight
   layout false
-  def flight
-    respond_to do |format|
-      format.html
-      format.js { redirect_to root_path }
-    end
-  end
 end
