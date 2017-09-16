e9d6edaa0f08913a528d1b8bc47d9a3a520c9edc
pookzzz2231 <w.kuttiya@gmail.com>
2017-09-16 01:18:53 -0400

    move js

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module RailsWidget
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    asset_path('widgets.css', digest: false) 
   end
 end
